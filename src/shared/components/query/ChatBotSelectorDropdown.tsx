55a5098db5b58a7469bcc23520604aa8b76041a3
oplantalech <oleguer@thehyve.nl>
2025-06-03 10:53:20 +0200

    WIP

--- src/shared/components/query/ChatBotSelectorDropdown.tsx
+++ src/shared/components/query/ChatBotSelectorDropdown.tsx
@@ -1,49 +1,30 @@
 import React from 'react';
 
-import levelStyles from './level.module.scss';
 import mainStyles from './main.module.scss';
 import collapsibleStyles from './collapsible.module.scss';
 import { Collapse } from 'react-collapse';
 import classnames from 'classnames';
 
 export type ChatBotSelectorDropdownProps = {
-    levels: string[];
-    levelDes: { [level: string]: JSX.Element };
+    collapsibleArea: JSX.Element;
+    dropdownTitle: string;
 };
 
 export const ChatBotSelectorDropdown: React.FunctionComponent<ChatBotSelectorDropdownProps> = (
     props: ChatBotSelectorDropdownProps
 ) => {
-    const [levelsCollapsed, updateLevelCollapse] = React.useState(true);
-
-    function levelListItem(level: string, levelDes: JSX.Element) {
-        return (
-            <li key={level} className={levelStyles['levels-li']}>
-                <i className="test" />
-                {levelDes}
-            </li>
-        );
-    }
-
-    function generateLevelRows(): JSX.Element[] {
-        const rows: JSX.Element[] = [];
-        props.levels.forEach(level => {
-            rows.push(levelListItem(level, props.levelDes[level]));
-        });
-
-        return rows;
-    }
+    const [componentCollapsed, updateLevelCollapse] = React.useState(true);
 
     return (
         <div>
             <div
                 data-test="chatboxselector-dropdown-header"
                 className={collapsibleStyles['collapsible-header']}
-                onClick={() => updateLevelCollapse(!levelsCollapsed)}
+                onClick={() => updateLevelCollapse(!componentCollapsed)}
             >
-                Ask ChatBOtQL
+                {props.dropdownTitle}
                 <span style={{ float: 'right' }}>
-                    {levelsCollapsed ? (
+                    {componentCollapsed ? (
                         <i
                             className={classnames(
                                 'fa fa-chevron-down',
@@ -60,22 +41,8 @@ export const ChatBotSelectorDropdown: React.FunctionComponent<ChatBotSelectorDro
                     )}
                 </span>
             </div>
-            <Collapse isOpened={!levelsCollapsed}>
-                <div
-                    className={classnames(
-                        levelStyles.levels,
-                        collapsibleStyles['levels-collapse']
-                    )}
-                >
-                    <ul
-                        style={{
-                            lineHeight: 8,
-                            padding: 0,
-                        }}
-                    >
-                        {generateLevelRows()}
-                    </ul>
-                </div>
+            <Collapse isOpened={!componentCollapsed}>
+                {props.collapsibleArea}
             </Collapse>
         </div>
     );
